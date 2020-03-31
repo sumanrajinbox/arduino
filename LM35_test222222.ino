54494ce2a5a8035f258083ec3c0b5339e7068290
suman raj <sumanrajinbox@gmail.com>
2020-03-31 08:09:42 +0530

    Add files via upload

---
+++ LM35_test222222.ino
@@ -0,0 +1,43 @@
+const int lm35_pin = A1;  /* LM35 O/P pin */
+int led = 9;           // the pin that the LED is attached to
+void setup() {
+  Serial.begin(9600);
+ pinMode(led, OUTPUT);
+  
+}
+
+void loop() {
+float temp_val ;
+  temp_val =  read_temperature(lm35_pin);
+
+   int temp_adc_val;
+  float temp_val;
+  temp_adc_val = analogRead(lm35_pin); /* Read Temperature */
+  temp_val = (temp_adc_val * 4.88); /* Convert adc value to equivalent voltage */
+  temp_val = (temp_val/10); /* LM35 gives output of 10mv/°C */
+//  Serial.print("Temperature = ");
+//  Serial.print(temp_val);
+//  Serial.print(" Degree Celsius\n");
+  
+led_function(led,255);
+ 
+  delay(200);
+}
+
+int read_temperature(int lm35_pin){ 
+  int temp_adc_val;
+  float temp_val;
+  temp_adc_val = analogRead(lm35_pin); /* Read Temperature */
+  temp_val = (temp_adc_val * 4.88); /* Convert adc value to equivalent voltage */
+  return  temp_val = (temp_val/10); /* LM35 gives output of 10mv/°C */
+}
+
+void led_function(int led,float brightness){
+  float x ;
+  int fadeAmount = 1;
+  while(x == brightness){
+      brightness = brightness + fadeAmount;
+     analogWrite(led, brightness);
+    }
+  
+  }
